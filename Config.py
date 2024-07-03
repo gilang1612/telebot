449f54c659a24fa23590ac0d774b9bb10c5d2f76
akmal520 <akmalbentar15@gmail.com>
2024-07-03 18:05:16 +0700

    iya

--- Config.py
+++ Config.py
@@ -3,5 +3,7 @@ import os
 token = os.environ.get(
     "BOT_TOKEN", "MASUKIN TOKEN BOTMU DISINI")  # BUAT BOT DAN AMBIL TOKEN BOT DI https://t.me/BotFather
 
+# SET DEBUG KE FALSE JIKA TIDAK INGIN MODE DEBUG
 debug = os.environ.get("DEBUG", "True")
-nama_bot = os.environ.get("NAMA_BOT", "NAMA BOT KALIAN")
+
+nama_bot = os.environ.get("NAMA_BOT", "NAMA BOT KALIAN")  # NAMA BOT
